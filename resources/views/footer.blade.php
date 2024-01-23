90d7178c9dc783025266cad37cbf249836944074
naul99 <luanboss012@gmail.com>
2024-01-24 02:26:32 +0700

    #syn theme

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,83 +1,39 @@
-<footer id="footer" class="clearfix">
-    <style>
-        .footers a {
-            font-size: 14px;
-            color: #696969;
-        }
-
-        .footers p {
-            font-size: 14px;
-            color: #696969;
-        }
-
-        .footers ul {
-            line-height: 30px;
-        }
-
-
-
-        @media (max-width: 480px) {
-            .img-footer {
-                width: 30%;
-            }
-        }
-
-        @media (min-width: 481px) {
-            .img-footer {
-                width: 100%;
-            }
-        }
-    </style>
-    <section class="footers bg-light pt-5 pb-3">
-        <div class="container pt-5">
-            <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-4 footers-one">
-                    <div class="footers-logo">
-                        <img src="{{ asset('uploads/logo/' . $info->logo) }}" alt="Logo" style="width:120px;">
-                    </div>
-                    <div class="footers-info mt-3">
-                        <p>{{ $info->title }}</p>
-                    </div>
-
-                </div>
-              
-                {{-- <div class="col-xs-12 col-sm-6 col-md-2 footers-two">
-                    <img class="img-footer" src="{{ asset('uploads/images/logodisney.png') }}" alt="Disney">
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-2 footers-three">
-                    <img class="img-footer" src="{{ asset('uploads/images/Marvel.png') }}" alt="Marvel">
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-2 footers-four">
-                    <img class="img-footer" src="{{ asset('uploads/images/DC.png') }}" alt="DC">
-                </div>
-                <div class="col-xs-12 col-sm-6 col-md-2 footers-five">
-                    <img class="img-footer" src="https://seeklogo.com/images/U/Universal-logo-72A5C164C2-seeklogo.com.png"
-                        alt="Universal Pictures"> --}}
-               
+<footer class="mainfooter d-flex direction-column space-between" id=" footer">
+    <div class="container footer-container flex-start">
+        <div class="widgets d-flex space-between">
+            <div class="first-widget">
+                <ul>
+                    <li class="list-item">Audio and Subtitles</li>
+                    <li class="list-item">Media Center</li>
+                    <li class="list-item">Privacy</li>
+                    <li class="list-item">Contact us</li>
+                </ul>
             </div>
+            <div class="second-widget">
+                <ul>
+                    <li class="list-item">Help Center</li>
+                    <li class="list-item">Cookie</li>
+                    <li class="list-item">Jobs</li>
+                </ul>
+            </div>
+            <div class="third-widget">
+                <ul>
+                    <li class="list-item">Audio Description</li>
+                    <li class="list-item">Investor Relations</li>
+                    <li class="list-item">Legal Notice</li>
+                </ul>
+            </div>
+            <div class="forth-widget">
+                <ul>
+                    <li class="list-item">Gift Card</li>
+                    <li class="list-item">Term Of Use</li>
+                    <li class="list-item">Corporate Information</li>
+                </ul>
             </div>
         </div>
-    </section>
-    <section class="disclaimer bg-light border">
-        <div class="container">
-            <div class="row ">
-                <div class="col-md-12 py-2">
-                    <small style="font-size: 15px">
-                        {{ $info->description }}
-                    </small>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="copyright border">
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-md-12 pt-3">
-                    <p class="text-muted"> © {{ now()->year }} Copyright:
-                        <a href="{{ route('homepage') }}" class="text-white">FULLHD Phim</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </section>
+        <button class="button service">Service Code</button>
+        <p class="copyright">@copyright 2020 Vanilacodes, Inc.</p>
+    </div>
+
+
 </footer>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,139 +1,77 @@
-<div class="navbar-container">
-    <div class="container">
-        <nav class="navbar halim-navbar main-navigation" role="navigation" data-dropdown-hover="1">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#halim"
-                    aria-expanded="false">
-                    <span class="sr-only">Menu</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-              
-                <button type="button" class="navbar-toggle collapsed pull-right get-bookmark-on-mobile">
-                    Bookmarks <i class="fa fa-bookmark" aria-hidden="true"></i>
-                    <span class="count">0</span>
-                </button>
-               
-                   
-                {{-- @guest('customer')
+<header class="d-flex space-between flex-center flex-middle">
+    <div class="nav-links d-flex flex-center flex-middle">
+        <a href="/">
+            <h2 class="logo logo-text red-color f-s-28 m-r-25">NETFLIX</h2>
+            <h2 class="second-logo-text red-color f-s-28">N</h2>
+        </a>
+        <a href="/" class="nav-item home">Home</a>
 
-                <button type="button" class="navbar-toggle collapsed pull-right get-locphim-on-mobile">
-                    
-                    <a data-toggle="modal" data-target="#exampleModal1" style="color: #ced4da;">Login </a>
-                </button>
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal1">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div style="background-color: #1b2b3c;" class="modal-header">
-                                <h3 style="color: #ffff;" class="modal-title text-center">
-                                    LOGIN:
-                                </h3>
-                               
-                            </div>
-                            <div style="background-color: #1b2b3c;" class="modal-body">
-                                <div>
-                                    <a href="{{ route('social-github-login') }}" class="btn-login-with bg3 m-b-10 delete">
-                                        <i class="fa-brands fa-github"></i>
-                                        Login with Github
-                                    </a>
-                                    <a href="{{ route('social-facebook-login') }}" class="btn-login-with bg1 m-b-10">
-                                        <i class="fa-brands fa-facebook"></i>
-                                        Login with Facebook
-                                    </a>
-                
-                                    <a href="{{ route('social-google-login') }}" class="btn-login-with bg2 m-b-10">
-                                        <i class="fa-brands fa-google"></i>
-                                        Login with Google
-                                    </a>
-                                </div>
-                            </div>
-                            <div style="background-color: #1b2b3c;" class="modal-footer">
-                                <button type="button" class="btn btn-danger" data-dismiss="modal">
-                                    Close
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                @else
-
-              
-                <button class="navbar-toggle collapsed pull-right get-locphim-on-mobile" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {{ Auth::guard('customer')->user()->name }}
-                </button>
-              
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <li><a class="dropdown-item" href="{{ route('register-package') }}">Đăng ký gói phim</a></li>
-                    <li><a class="dropdown-item" href="{{ route('history-order') }}">Đăng ký gói phim</a></li>
-                    <li><a class="dropdown-item" href="{{ route('history') }}">History</a></li>
-                     <li><a class="dropdown-item" href="{{ route('sociallogout') }}">Logout</a></li>
-                 
-                </ul>
-                @endguest --}}
-                
-            </div>
-            <div class="collapse navbar-collapse" id="halim">
-                <div class="menu-menu_1-container">
-                    <ul id="menu-menu_1" class="nav navbar-nav navbar-left">
-                        <li class="current-menu-item active"><a title="Trang Chủ" href="{{ route('homepage') }}">Trang
-                                Chủ</a></li>
-                        @foreach ($category as $key => $cate)
-                            <li class="mega"><a title="{{ $cate->title }}"
-                                    href="{{ route('category', $cate->slug) }}">{{ $cate->title }}</a></li>
-                        @endforeach
-
-                        <li class="mega dropdown">
-                            <a title="Thể Loại" href="#" data-toggle="dropdown" class="dropdown-toggle"
-                                aria-haspopup="true">Thể Loại <span class="caret"></span></a>
-                            <ul role="menu" class=" dropdown-menu">
-                                @foreach ($genre as $key => $gen)
-                                    <li><a title="{{ $gen->title }}"
-                                            href="{{ route('genre', $gen->slug) }}">{{ $gen->title }}</a></li>
-                                @endforeach
-                            </ul>
-                        </li>
-                        <li class="mega dropdown">
-                            <a title="Quốc Gia" href="#" data-toggle="dropdown" class="dropdown-toggle"
-                                aria-haspopup="true">Quốc Gia <span class="caret"></span></a>
-                            <ul role="menu" class=" dropdown-menu">
-                                @foreach ($country as $key => $count)
-                                    <li><a title="{{ $count->title }}"
-                                            href="{{ route('country', $count->slug) }}">{{ $count->title }}</a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </li>
-                        <li class="mega dropdown">
-                            <a title="Năm phim" href="#" data-toggle="dropdown" class="dropdown-toggle"
-                                aria-haspopup="true">Năm phim <span class="caret"></span></a>
-                            <ul role="menu" class=" dropdown-menu">
-                                <li><a href="{{ url('nam/more') }}">Khác</a>
-                            </li>
-                                @for ($year = 2000; $year <= now()->year; $year++)
-                                    <li><a title="{{ $year }}"
-                                            href="{{ url('nam/' . $year) }}">{{ $year }}</a>
-                                    </li>
-                                @endfor
-                            </ul>
-                        </li>
-                        <!-- <li><a title="Phim Lẻ" href="danhmuc.php">Phim Lẻ</a></li>
-                    <li><a title="Phim Bộ" href="danhmuc.php">Phim Bộ</a></li>
-                    <li><a title="Phim Chiếu Rạp" href="danhmuc.php">Phim Chiếu Rạp</a></li> -->
-                    </ul>
-                </div>
-                <ul class="nav navbar-nav navbar-left" style="background:#000;">
-                    <li><a href="{{ route('all-movies') }}" style="color: #ffed4d;">Tất Cả Phim</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class="collapse navbar-collapse" id="search-form">
-            <div id="mobile-search-form" class="halim-search-form"></div>
-        </div>
-        <div class="collapse navbar-collapse" id="user-info">
-            <div id="mobile-user-login"></div>
-        </div>
+        @foreach ($category as $key => $cate)
+            <a href="{{ route('category', $cate->slug) }}" class="nav-item">{{ $cate->title }}</a>
+        @endforeach
+       
+        
+        <a href="latest.html" class="nav-item latest">Latest</a>
+        <a href="mylist.html" class="nav-item">My List</a>
     </div>
-</div>
+    <div class="righticons d-flex flex-end flex-middle">
+        <a href="search.html"><img src="../images/icons/search.svg" alt="search icon"></a>
+        <div class="dropdown notification">
+            <img src="../images/icons/notification.svg" alt="notificatio icon">
+            <div class="dropdown-content">
+                <a href="#" class="profile-item d-flex flex-middle">
+                    <img src="../../images/icons/user2.png" alt="user profile icon" class="user-icon">
+                    <span>You have new notification from <span>User 123</span></span>
+                </a>
+                <a href="#" class="profile-item d-flex flex-middle">
+                    <img src="../../images/icons/user1.png" alt="user profile icon" class="user-icon">
+                    <span>You have new notification from <span>User 123</span></span>
+                </a>
+                <a href="#" class="profile-item d-flex flex-middle">
+                    <img src="../../images/icons/user4.png" alt="user profile icon" class="user-icon">
+                    <span>You have new notification from <span>User 123</span></span>
+                </a>
+                <a href="#" class="profile-item d-flex flex-middle">
+                    <img src="../../images/icons/user3.png" alt="user profile icon" class="user-icon">
+                    <span>You have new notification from <span>User 123</span></span>
+                </a>
+            </div>
+        </div>
+
+        <div class="dropdown">
+            <img src="../images/icons/user-image-green.png" alt="user profile icon" class="user-icon">
+            <span class="profile-arrow"></span>
+
+            <div class="dropdown-content">
+                <div class="profile-links">
+                    <a href="#" class="profile-item d-flex flex-middle">
+                        <img src="../images/icons/user1.png" alt="user profile icon" class="user-icon">
+                        <span>Rajesh</span>
+                    </a>
+                    <a href="#" class="profile-item d-flex flex-middle">
+                        <img src="../images/icons/user2.png" alt="user profile icon" class="user-icon">
+                        <span>Karan</span>
+                    </a>
+                    <a href="#" class="profile-item d-flex flex-middle">
+                        <img src="../images/icons/user3.png" alt="user profile icon" class="user-icon">
+                        <span>Pappy</span>
+                    </a>
+                    <a href="#" class="profile-item d-flex flex-middle"
+                        style="margin-bottom: 13px;">
+                        <img src="../images/icons/user4.png" alt="user profile icon" class="user-icon">
+                        <span>Denny</span>
+                    </a>
+                    <a href="#" class="profile-item last">Manage Profiles</a>
+                </div>
+                <div class="line"></div>
+                <div class="links d-flex direction-column">
+                    <a href="user-profile/home.html">Account</a>
+                    <a href="#">Help Center</a>
+                    <a href="/">Sign Out of Netflix</a>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</header>
